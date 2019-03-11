a535a1b98c6b840d5ae7d63d1387628ad98edfaf
dferlay <ferlay10069@jbdelasalle.com>
2019-03-11 11:34:16 +0100

    Lol

---
+++ html/accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = 'utf-8'>
+		<title> Accueil </title>
+		<link rel='stylesheet' href='style.css'>
+	</head>
+	
+	<body>
+		<div class = 'header'>
+			<ul>
+				<li><a>Connexion</a></li>
+				<li><a>Inscription</a></li>
+			<ul>
+		</div>
+	</body>
+</html>
