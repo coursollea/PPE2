8c270bb0369984d10ea45ba9fdd3fa2eca39c613
dferlay <ferlay10069@jbdelasalle.com>
2019-03-13 13:38:14 +0100

    Mise à jour 13/03

--- html/accueil.php
+++ html/accueil.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset = 'utf-8'>
@@ -13,5 +13,30 @@
 				<li><a>Inscription</a></li>
 			<ul>
 		</div>
+		
+			<nav>
+				<ul>
+					<li><a>Accueil</a></li>
+					<li><a>Se présenter</a></li>
+					<li><a>Requête</a></li>
+					<li><a>Contact</a></li>
+				</ul>
+			</nav>
+			<nav class = 'subNav'>
+				<ul>
+					<li><a> Général </a></li>
+					<li><a> Jeu vidéo </a></li>
+					<li><a> Musique </a></li>
+					<li><a> Film </a></li>
+					<li><a> Développement </a></li>
+					
+				<ul>
+			</nav>
+			<article>
+				<h1> Accueil </h1>
+				
+				<div>
+				</div>
+			</article>
 	</body>
 </html>
