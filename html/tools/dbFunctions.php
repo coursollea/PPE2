6cb0d0d1c21b74a728359f57c8a4844c4c57fae3
coursollea <arthur.coursolle@gmail.com>
2019-04-08 11:11:23 +0200

    le mockup de Bannir/Bloquer pour l'administrateur

--- html/tools/dbFunctions.php
+++ html/tools/dbFunctions.php
@@ -3,13 +3,11 @@
 	// Créer un PDO ( A UTILISER IMPERATIVEMENT A CHAQUE FOIS QU'ON A BESOIN DE CREER UN PDO )
 	function createPDO()
 	{
-<<<<<<< HEAD
+
 		return new PDO('mysql:host=localhost;dbname=basesite;charset=utf8', 'root', 'root');
 	}
-=======
-		return new PDO('mysql:host=localhost;dbname=basesite;charset=utf8', 'root', '');
-	}.0
->>>>>>> 8e5b03dd8c150498abeaee0d6be8b78f1177f6d3
+
+	
 	
 	// TRUE si l'utilisateur est connecté, sinon FALSE
 	function isConnected()
@@ -222,7 +220,7 @@
 				</div></a>";
 	}
 	
-<<<<<<< HEAD
+
 	//Renvoie l'id de l'utilisateur
 	
 	/*function getUserId($username)
@@ -233,7 +231,7 @@
 		
 		return $reponse->fetch(PDO::FETCH_ASSOC);
 	}*/
-=======
+
 	
 	// Renvoie le pseudo d'un utilisateur a partir de son id
 	function getUserNameOf($idCompte)
@@ -263,4 +261,4 @@
 		$_SESSION['username'] = getUserNameOf($_SESSION['idCompte']);
 	}
 	
->>>>>>> c102d58fa17cb2a80aa06107a2894515672aa1d7
+
--- html/tools/fonctionPage.php
+++ html/tools/fonctionPage.php
@@ -3,11 +3,9 @@ include('dbFunctions.php');
 function LocalisePage($nomPage)
 	{
 		$bdd = createPDO();
-<<<<<<< HEAD
 		$query = 'SELECT label FROM categorie Where nomCategorie  = "'.$nomPage.'";';
 		$reponse = $bdd->query($query);
 		return  $reponse->fetch(PDO::FETCH_ASSOC)["label"];
-=======
 		
 		$query = 'SELECT label FROM categorie Where nomCategorie = ?';
 		$statement = $bdd->prepare($query);
@@ -18,7 +16,6 @@ function LocalisePage($nomPage)
 		
 		$label = $statement->fetch(PDO::FETCH_ASSOC);
 		return $label['label'];
->>>>>>> c102d58fa17cb2a80aa06107a2894515672aa1d7
 	}
 
 ?>
