18673e79457e7e7551e6a4ad6bde9c90e4b48485
kuroi <thomas.helligar@live.fr>
2019-04-29 10:44:13 +0200

    correction

--- html/tools/dbFunctions.php
+++ html/tools/dbFunctions.php
@@ -7,10 +7,7 @@
 		return new PDO('mysql:host=localhost;dbname=basesite;charset=utf8', 'root', 'root');
 	}
 
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/origin/master
 	// TRUE si l'utilisateur est connecté, sinon FALSE
 	function isConnected()
 	{
@@ -237,6 +234,7 @@
 		'</a>';
 		
 		return $html;
+	}
 
 
 
@@ -293,6 +291,3 @@
 		}
 		return $tab;
 	}
-	
-
-
